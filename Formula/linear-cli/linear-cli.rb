b0a7ce5f9b0c8d8edafcc5fd4ccd0875738c972d
ruby-automation <171511148+ruby-automation@users.noreply.github.com>
2026-08-20 12:17:21 -0400

    fix: bump linear-cli formula to v1.16.0 (#26)
    Co-authored-by: bougyman <6848+bougyman@users.noreply.github.com>

--- Formula/linear-cli/linear-cli.rb
+++ Formula/linear-cli/linear-cli.rb
@@ -13,19 +13,19 @@ class LinearCli < Formula
   on_macos do
     on_arm do
       url "https://github.com/rubyists/linear-cli/releases/download/v#{version}/lc_macos_aarch64.tar.gz"
-      sha256 "aee81b4c949f423cbbc229604cdce92f45785cdbe408e6dff3cc98317089e7e7"
+      sha256 "d534c5508fae8f4d66e2a27a76d5dc19a037c57263294a29345d324118be8d73"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/rubyists/linear-cli/releases/download/v#{version}/lc_linux_x86_64.tar.gz"
-      sha256 "2b2b28d16eebf53f2b53dbeb1618ba9fd9c5f0aed52467171e53cb2ab182d022"
+      sha256 "a0aa03e8fa11a48c96eb9ebcf60c7814b2c1f7bbf5fa7912f7c2c5dfa32ee9e1"
     end
 
     on_arm do
       url "https://github.com/rubyists/linear-cli/releases/download/v#{version}/lc_linux_aarch64.tar.gz"
-      sha256 "a92dbcbe87eb723e0a5734347bd757c9768684e10414cbfcef84e4e2e7ed0d20"
+      sha256 "0ce142810a02fb6445183105137ed1e16bd4e60d0be917a5d86866c608a3c42c"
     end
   end
 
